71cbdba96ff480bade52b9da1289c2a541d58e57
illakaya <mimijane.nguyen@gmail.com>
2024-06-04 17:07:10 +1000

    added code to delete items from db

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,8 +48,18 @@ router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deletedCategory);
+  } catch (error) {
+    res.status(404).json(error);
+  }
 });
 
 module.exports = router;
